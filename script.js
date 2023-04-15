22c61713b06cd7793c864a5ca865f19a33751e01
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-04-14 23:30:18 -0400

    add basic functions, almost done

--- script.js
+++ script.js
@@ -10,4 +10,72 @@ function getComputerChoice () {
         return "scissors";
     }
 }
-console.log(getComputerChoice());
+
+function answerChecker (a) {
+    if (a == "rock" || a == "paper" || a == "scissors") {
+        return a;
+    }
+    else {
+        alert("that's not a valid option... google the rules maybe? Idk. Idc.")
+    }
+}
+
+function rockf (a, b) {
+    if (a == "rock") {
+        tieGame(a, b);
+    }
+    else if (a == "paper") {
+        youLose(a, b);
+    }
+    else {
+        youWin(a,b);
+    }
+}
+
+function paperf (b, a) {
+    if (a == "rock") {
+
+    }
+    else if (a == "paper") {
+
+    }
+    else {
+        
+    }
+}
+
+function scissorsf (b, a) {
+    if (a == "rock") {
+
+    }
+    else if (a == "paper") {
+
+    }
+    else {
+        
+    }
+}
+
+
+let computerSelection = getComputerChoice();
+
+let getPlayerSelection = prompt("Choose rock, paper, or scissors.");
+
+let playerSelection = answerChecker(getPlayerSelection.toLowerCase());
+
+switch (playerSelection) {
+    case "rock":
+        rockf (computerSelection, playerSelection);
+        break;
+    case "paper":
+        paperf (computerSelection, playerSelection);
+        break;
+    case "scissors":
+        scissorsf (computerSelection, playerSelection);
+        break;
+    default:
+        prompt("something went wrong, Josh needs to write better code!");
+}
+
+console.log("Computer has selected " + computerSelection);
+console.log("You have selected " + playerSelection);
